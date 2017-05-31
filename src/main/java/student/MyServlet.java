174b209e0091adec4347d2b473923c0d43109362
minyaylo <ihor.minyaylo@gmail.com>
2017-05-31 12:01:40 +0300

    'For Mykola Halchuk'

---
+++ src/main/java/student/MyServlet.java
@@ -0,0 +1,29 @@
+package student;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by user on 30.05.2017.
+ */
+@WebServlet("/s")
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("Hello Word");
+    }
+}
